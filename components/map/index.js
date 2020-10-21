e6e22cff29864695d534bf7168b6cf6095525cad
github NikolaCzv <nikola.raicic@hotmail.com>
2020-10-21 15:19:32 -0500

    marker

--- components/map/index.js
+++ components/map/index.js
@@ -1,24 +1,41 @@
 import React, { useState } from 'react';
 import MapView from 'react-native-maps';
-import { StyleSheet, Dimensions, SafeAreaView } from 'react-native';
+import { Marker, Callout } from 'react-native-maps';
+import { StyleSheet, Dimensions, SafeAreaView, Text, Image, ScrollView } from 'react-native';
+import fireLogo from "../../assets/fire-logo.png"; 
 
 const DEFAULT_REGION = {
-    latitude: 42.00250679125294,
-    longitude: -87.67067112028599,
+    latitude: 41.7645358,
+    longitude: -87.8050120,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421
 };
 
+const DEFAULT_MARKERS = [
+	{
+		latlng: {
+			latitude: 41.7645358, 
+			longitude: -87.8050120
+		},
+		title: "Chicago Fire Soccer Club",
+		description: "Chicago Fire Football Club are an American professional soccer franchise based in Chicago, Illinois. The team competes in Major League Soccer (MLS) as a member of the league's Eastern Conference, having moved to the conference in 2002.",
+		logo: fireLogo
+	}
+];
+
 const Map = () => {
 
-    const [region, setRegion] = useState(DEFAULT_REGION);
+	const [region, setRegion] = useState(DEFAULT_REGION);
+	const [markers, setMarkers] = useState(DEFAULT_MARKERS);
+	const [screenHeight, setScreenHeight] = useState(0);
 
 	const onRegionChange = (region) => {
 		setRegion(region);
-    };
+	};
 
-    console.log("latitude", region.latitude)
-    console.log("longitude", region.longitude)
+	const handleonContentSizeChange = (contentWidth, contentHeight) => {
+		setScreenHeight(contentWidth);
+	};
 
 		return (
 			<SafeAreaView style={styles.container}>
@@ -28,7 +45,26 @@ const Map = () => {
 					provider={MapView.PROVIDER_GOOGLE}
 					region={region}
 					onRegionChangeComplete={(region) => onRegionChange(region)}
-				/>
+				>
+					{markers.map((marker, index) => (
+						<Marker 
+							key={index}
+							coordinate={marker.latlng}
+						>
+							<Callout style={styles.boxStyle}>
+								<ScrollView 
+									style={styles.scroll}
+									scrollEnabled={true}
+									onContentSizeChange={handleonContentSizeChange}
+									>
+									<Text style={styles.titleStyle}>{marker.title}</Text>
+									<Image source={marker.logo} style={styles.logoStyle}/>
+										<Text>{marker.description}</Text>
+								</ScrollView>
+							</Callout>
+						</Marker>
+					))}
+				</MapView>
 			</SafeAreaView>
 		)
 }
@@ -46,4 +82,22 @@ const styles = StyleSheet.create({
 		width: Dimensions.get('window').width,
 		height: Dimensions.get('window').height,
 	},
+	boxStyle: {
+		height: 200,
+		width: 200,
+		marginHorizontal: 20,
+		padding: 10
+	},
+	titleStyle: {
+		fontWeight: 'bold',
+		color: '#377DFC'
+	},
+	logoStyle: {
+		width: '90%',
+		height: 100,
+		margin: 10
+	},
+	scroll: {
+		width: '100%'
+	}
 });
--- containers/main/index.js
+++ containers/main/index.js
@@ -1,9 +1,28 @@
 import React from "react";
 import Map from "../../components/map";
+import { View, StyleSheet, Text} from "react-native";
 
 
 const Main = () => {
-    return <Map />
+    return <View style={styles.container}>
+                <Map />
+            </View>
 };
 
 export default Main;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    bottomMenu: {
+        height: 50,
+        width: 380,
+        backgroundColor: '#377DFC',
+        borderTopEndRadius: 10,
+        borderTopStartRadius: 10
+    }
+  });
